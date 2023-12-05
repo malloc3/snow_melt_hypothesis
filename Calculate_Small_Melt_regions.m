25cc6b3cc6af8f5dae1ef7b5fefa692813ff4e91
Cannon Mallory <mallorycannon15@gmail.com>
2023-12-04 16:31:41 -0800

    addition of some analysis scripts etc

--- Calculate_Small_Melt_regions.m
+++ Calculate_Small_Melt_regions.m
@@ -11,11 +11,12 @@ CSV_file_path = "/Users/Cannon/Documents/School/UCSB/Briggs Lab/Thaw_Rate_Hypoth
 %ROI_POINT = [37.07095335, -118.5436752];
 
 %Dates.  Should be improved but for now 1 is like late summer early fall
+
 start_day = 1;
 number_days = 364;
 regions_of_interest =  readtable(CSV_file_path);
 region_size = 500; %meters
-metavariable = 'swe';
+metavariable = 'sweHybrid';
 
 
 ROI_melts = [];
@@ -61,13 +62,77 @@ end
 %figure
 %plot(datenum(dates(start_day:(start_day+number_days - 1))), mean(ROI_melts(16:end,:)), '-o')
 %title('Persistant')
+ 
+
 real_dates = datetime(dates,'ConvertFrom','datenum');
+%figure
+%hold on
+%    plot(real_dates(1:1092), mean(ROI_Saftey(1:7,:)), '-o')
+%    plot(real_dates(1:1092), mean(ROI_Saftey(9:16,:)), '-o')
+%    plot(real_dates(1:1092), mean(ROI_Saftey(16:end,:)), '-o')
+%    legend('Decreasing','Naive', 'Persistent')
+%hold off
+
 figure
+plot(real_dates(1:1092), ROI_melts(1:7,:), '-o')
+title('Decreasing sweHybrid Melt 2019-2021')
+ylabel('sweHybrid Melt mm') 
+
+figure
+plot(real_dates(1:1092), ROI_melts(9:16,:), '-o')
+title('Naive sweHybrid Melt 2019-2021')
+ylabel('sweHybrid Melt mm') 
+
+figure
+plot(real_dates(1:1092), ROI_melts(17:end,:), '-o')
+title('Persistent sweHybrid Melt 2019-2021')
+ylabel('sweHybrid Melt mm') 
+
+figure
+dates_for_fill = real_dates(1:1091)';
+x2 = [dates_for_fill, fliplr(dates_for_fill)];
+
+[s_d, m_d] = std(rolling_average(1:7,:), 1, 1);
+[s_n, m_n] = std(rolling_average(9:16,:), 1, 1);
+[s_p, m_p] = std(rolling_average(17:end,:), 1, 1);
+
+curve1_d = m_d + s_d;
+curve2_d = m_d - s_d;
+inBetween_d = [curve1_d, fliplr(curve2_d)];
+
+curve1_n = m_n + s_n;
+curve2_n = m_n - s_n;
+inBetween_n = [curve1_n, fliplr(curve2_n)];
+
+curve1_p = m_p + s_p;
+curve2_p = m_p - s_p;
+inBetween_p = [curve1_p, fliplr(curve2_p)];
+
 hold on
-    plot(real_dates(1:1092), mean(ROI_fixed(1:7,:)), '-o')
-    plot(real_dates(1:1092), mean(ROI_fixed(9:16,:)), '-o')
-    plot(real_dates(1:1092), mean(ROI_fixed(16:end,:)), '-o')
-    legend('Decreasing','Naive', 'Persistant')
+    hp_d = fill(x2, inBetween_d, 'r');
+    set(hp_d, 'facecolor', 'r', 'edgecolor', 'r');
+
+    %hp_n = fill(x2, inBetween_n, 'b');
+    %set(hp_n, 'facecolor', 'b', 'edgecolor', 'b');
+
+
+    hp_p = fill(x2, inBetween_p, 'k');
+    set(hp_p, 'facecolor', 'k', 'edgecolor', 'k');
+    
+    alpha(hp_p, .2)
+    alpha(hp_d, .2)
+    %alpha(hp_n, .2)
+
+    hl_d = plot(real_dates(1:1091), m_d, '-o');
+    %hl_n = plot(real_dates(1:1092), m_n, '-o');
+    hl_p = plot(real_dates(1:1091), m_p, '-o');
+    set(hl_p, 'color', 'k', 'marker', 'x');
+    %set(hl_n, 'color', 'b', 'marker', 'x');
+    set(hl_d, 'color', 'r', 'marker', 'x');
+    legend('Decreasing', 'Persistent')
+
+    title('sweHybrid Melt 2019-2021')
+    ylabel('sweHybrid Melt mm') 
 hold off
 
 %This is not the fastest way to do this.... It would actually be much
